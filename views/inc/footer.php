1da715e5fa64720f8ea305058bdaffb188376c47
Thomas Bolot <tbolot.lamanu@gmail.com>
2021-05-12 11:00:26 +0200

    ajout nav footer et bootsttrap

---
+++ views/inc/footer.php
@@ -0,0 +1,48 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+  <link href="<?php echo $href ?>" rel="stylesheet">
+  <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"> -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+    integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
+    crossorigin="anonymous" />
+
+    <div class="footer">
+    <div class="container">
+      <div class="col">
+        <h1></h1>
+        <ul>
+          <li>A propos</li>
+
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+
+      <div class="col">
+        <h1></h1>
+        <ul>
+          <li></li>
+          <li></li>
+
+        </ul>
+      </div>
+
+      <div class="col">
+        <h1></h1>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+      <div class="col social">
+        <h1>Social</h1>
+        <ul>
+          <li><i class="fab fa-twitter"></i></li>
+          <li><i class="fab fa-instagram"></i></li>
+          <li><i class="fab fa-facebook-square"></i></li>
+
+        </ul>
+      </div>
+
+    </div>
